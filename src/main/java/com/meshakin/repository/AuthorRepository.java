4a263ab72aac84a7da6b53127a184932e47f2a2c
Мешакин Максим Александрович <mesakinmaksim@gmail.com>
2025-11-12 17:58:47 +0300

    add repositories

---
+++ src/main/java/com/meshakin/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.meshakin.repository;
+
+import com.meshakin.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+    Author findByName(String name);
+}
