3099955cfebab55a87f02c12657370b37258897d
Мешакин Максим Александрович <mesakinmaksim@gmail.com>
2025-11-12 17:57:34 +0300

    update entities

--- src/main/java/com/meshakin/entity/Book.java
+++ src/main/java/com/meshakin/entity/Book.java
@@ -5,13 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@EqualsAndHashCode(exclude = {"author", "genre"})
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
 
--- src/main/java/com/meshakin/entity/Genre.java
+++ src/main/java/com/meshakin/entity/Genre.java
@@ -4,7 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@EqualsAndHashCode
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Genre {
     @Id
